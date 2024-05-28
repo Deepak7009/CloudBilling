841acf73ea992d5d8124ec4cb045047046b81a87
vinunarwal <vinunarwal3@gmail.com>
2024-05-28 10:27:32 +0530

    Billing Logic

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -187,7 +187,7 @@ function RestaurantManagementApp() {
                   <div className="flex flex-col space-y-4">
                      <div className="flex flex-row justify-between bg-white p-4">
                         <p className="text-lg font-bold">{selectedCategory}</p>
-                        <button className="text-blue-500 font-bold">See All</button>
+                        {/*<button className="text-blue-500 font-bold">See All</button>*/}
                      </div>
                      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                         {filteredItems?.map((item) => (
@@ -210,40 +210,53 @@ function RestaurantManagementApp() {
                </div>
                <div className="flex flex-col w-full md:w-1/3 bg-white p-4 rounded shadow-md mt-4 md:mt-0 md:ml-4">
                   <p className="text-lg font-bold mb-4">Billing Details</p>
-                  <div className="flex flex-col space-y-2 mb-4">
-                     <input
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        value={billingDetails.name}
-                        onChange={handleBillingChange}
-                        className="border border-gray-400 rounded px-4 py-2"
-                     />
-                     <input
-                        type="text"
-                        name="mobile"
-                        placeholder="Mobile No."
-                        value={billingDetails.mobile}
-                        onChange={handleBillingChange}
-                        className="border border-gray-400 rounded px-4 py-2"
-                     />
-                     <input
-                        type="text"
-                        name="address"
-                        placeholder="Address"
-                        value={billingDetails.address}
-                        onChange={handleBillingChange}
-                        className="border border-gray-400 rounded px-4 py-2"
-                     />
-                     <input
-                        type="text"
-                        name="locality"
-                        placeholder="Locality"
-                        value={billingDetails.locality}
-                        onChange={handleBillingChange}
-                        className="border border-gray-400 rounded px-4 py-2"
-                     />
+                  <div className="flex flex-col space-y-4 mb-4">
+                     <div className='flex items-center'>
+                        <label className="w-1/4 text-right pr-4">Name:</label>
+                        <input
+                           type="text"
+                           name="name"
+                           placeholder="Name"
+                           value={billingDetails.name}
+                           onChange={handleBillingChange}
+                           className="border border-gray-400 rounded px-4 py-2 flex-grow"
+                        />
+                     </div>
+                     <div className='flex items-center'>
+                        <label className="w-1/4 text-right pr-4">Mobile:</label>
+                        <input
+                           type="text"
+                           name="mobile"
+                           placeholder="Mobile No."
+                           value={billingDetails.mobile}
+                           onChange={handleBillingChange}
+                           className="border border-gray-400 rounded px-4 py-2 flex-grow"
+                        />
+                     </div>
+                     <div className='flex items-center'>
+                        <label className="w-1/4 text-right pr-4">Add:</label>
+                        <input
+                           type="text"
+                           name="address"
+                           placeholder="Address"
+                           value={billingDetails.address}
+                           onChange={handleBillingChange}
+                           className="border border-gray-400 rounded px-4 py-2 flex-grow"
+                        />
+                     </div>
+                     <div className='flex items-center'>
+                        <label className="w-1/4 text-right pr-4">Locality:</label>
+                        <input
+                           type="text"
+                           name="locality"
+                           placeholder="Locality"
+                           value={billingDetails.locality}
+                           onChange={handleBillingChange}
+                           className="border border-gray-400 rounded px-4 py-2 flex-grow"
+                        />
+                     </div>
                   </div>
+
                   <p className="text-lg font-bold mb-4">Order Summary</p>
                   <div className="overflow-x-auto">
                      <table className="min-w-full divide-y divide-gray-200">
