2369cadb1a2b354c3318581c9646e5a2ec50bf71
ashishbahar <ashishbahar987@gmail.com>
2024-06-03 11:27:04 +0530

    ..

--- src/components/BillingDetails.jsx
+++ src/components/BillingDetails.jsx
@@ -16,6 +16,11 @@ const BillingDetails = ({
     const billData = {
       name: billingDetails.name,
       mobile: billingDetails.mobile,
+      orderItems: orderItems.map((item) => ({
+        name: item.name,
+        quantity: item.quantity,
+        price: item.price * item.quantity,
+      })),
     };
 
     try {
