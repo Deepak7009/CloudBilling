819e4ec9093fc2191492ce2ff1de3780140ccec1
ashishbahar <ashishbahar987@gmail.com>
2024-06-05 14:32:42 +0530

    hgf

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,31 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const SearchBar = ({
-
   selectedCategory,
   setSelectedCategory,
   searchQuery,
   setSearchQuery,
   items,
-
 }) => {
   const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+  const searchBarRef = useRef(null);
 
   const toggleSearchBar = () => {
     setIsSearchBarOpen(!isSearchBarOpen);
   };
 
+  const handleClickOutside = (event) => {
+    if (searchBarRef.current && !searchBarRef.current.contains(event.target)) {
+      setIsSearchBarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSearchBarOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSearchBarOpen]);
+
   return (
-    <div className="flex flex-row justify-between bg-white p-4 mb-4">
-      <p className="text-lg font-bold">Search Items</p>
+    <div
+      ref={searchBarRef}
+      className="flex relative flex-row justify-between bg-white p-4 mb-4"
+    >
+      <p
+        className={`text-lg font-bold ${isSearchBarOpen ? "hidden" : "block"}`}
+      >
+        Search Items
+      </p>
       <div className="flex items-center space-x-2">
         <div className="lg:hidden">
-          <button onClick={toggleSearchBar} className="text-2xl">
+          <button
+            onClick={toggleSearchBar}
+            className={`text-2xl ${isSearchBarOpen ? "hidden" : "block"}`}
+          >
             <i className="fas fa-search"></i>
           </button>
         </div>
         {isSearchBarOpen && (
-          <div className="flex flex-col lg:flex-row items-center space-y-2 lg:space-y-0 lg:space-x-2">
+          <div className="flex flex-col lg:hidden lg:flex-row items-center space-y-2 lg:space-y-0 lg:space-x-2">
             {/* <select
               className="border border-gray-400 rounded px-4 py-2"
               onChange={(e) => {
@@ -61,9 +88,7 @@ const SearchBar = ({
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           />
-       
         </div>
-
       </div>
     </div>
   );
