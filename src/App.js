8bed16fb0674fd63923f65ff864afbabb96def17
Deepak7009 <deepak7009verma@gmail.com>
2024-06-10 10:31:35 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import OrderHistory from "./components/OrderHistory";
 import GenerateCSV from "./components/GenerateCSV";
 import Login from "./login&register/Login";
 import Register from "./login&register/Register";
+import CustomTable from "./components/CustomTable";
+import Structure from "./components/Structure";
+import Process from "./components/Process";
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
         <Route path="/" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/category" element={<Category />} />
+        <Route path="/table" element={<CustomTable />} />
+        <Route path="/process" element={<Process />} />
+        <Route path="/structure" element={<Structure />} />
         <Route path="/history" element={<OrderHistory />} />
       </Routes>
 
