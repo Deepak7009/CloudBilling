61ec7f8d14a0332ca36c76ad11791bc2b614a076
vinunarwal <vinunarwal3@gmail.com>
2024-06-18 13:13:34 +0530

    Search Functionality Added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,11 @@ import axios from 'axios';
 import { baseUrl } from '../utils/Const';
 import { ItemsContext } from '../context/ItemsContext';
 import { jwtDecode } from 'jwt-decode';
-import { Link } from 'react-router-dom';
 
 const Sidebar = ({ setSelectedCategory, setSearchQuery }) => {
    const [categories, setCategories] = useState([]);
    const { setItems } = useContext(ItemsContext);
    const [userId, setUserId] = useState("");
-
 
    useEffect(() => {
       const token = localStorage.getItem('token');
@@ -28,6 +26,7 @@ const Sidebar = ({ setSelectedCategory, setSearchQuery }) => {
             const uniqueCategories = [...new Set(response.data.map(item => item.category))];
             setCategories(uniqueCategories);
 
+            // Set default selected category and fetch its products
             if (uniqueCategories.includes('Beverages')) {
                setSelectedCategory('Beverages');
                fetchProducts('Beverages');
@@ -63,12 +62,11 @@ const Sidebar = ({ setSelectedCategory, setSearchQuery }) => {
       try {
          const response = await axios.get(`${baseUrl}get-product-data/${userId}`);
          setItems(response.data);
-         setSearchQuery("");
+         setSearchQuery("");  // Reset search query when fetching all products
       } catch (error) {
          console.error("Error fetching all products:", error);
       }
    };
-
 
    return (
       <div className="flex flex-col lg:max-w-[210px] min-[767px]:max-w-[150px] w-full bg-white p-4 rounded shadow-md mb-4 md:mb-0 md:mr-4">
@@ -79,33 +77,19 @@ const Sidebar = ({ setSelectedCategory, setSearchQuery }) => {
          </div>
          <div className="flex overflow-x-auto md:overflow-y-auto">
             <ul className="flex md:flex-col">
-               {categories.length === 0 && (
-                  <div>
-                     <p className="mt-2 mb-4 text-center max-[]px-2 text-[17px] font-semibold text-gray-600 font-serif">
-                        No categories found
-                     </p>
-                     <Link to="/categories"
-                        className='flex items-center max-[]px-2 text-[17px] font-semibold text-gray-600 font-serif hover:underline underline-offset-4 hover:text-teal-600'
-                     >
-                        Add Category
-                        <span>
-                           <svg className="w-6 h-6 text-teal-600 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                              <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z" clip-rule="evenodd" />
-                           </svg>
-                        </span>
-                     </Link>
-                  </div>
-               )}
-
-               {categories.length > 0 && (
-                  <li className="mt-2 max-[]px-2 sidebar-item md:pb-2 md:ms-0 md:text-[20px] text-[17px] font-semibold font-serif">
-                     <button onClick={fetchAllProducts}>
-                        All Categories
-                     </button>
-                  </li>
-               )}
-
-               {categories.map((category, index) => (
+               <li className="mt-2 max-[]px-2 sidebar-item md:pb-2 md:ms-0 md:text-[20px] text-[17px] font-semibold font-serif">
+                  <button
+                     className="text-left w-full"
+                     onClick={() => {
+                        setSelectedCategory('All'); // Set selected category to 'All'
+                        setSearchQuery(""); // Clear search query
+                        fetchAllProducts(); // Fetch all products
+                     }}
+                  >
+                     All Categories
+                  </button>
+               </li>
+               {categories?.map((category, index) => (
                   <li
                      key={index}
                      className="mt-2 max-[]px-2 sidebar-item md:pb-2 md:ms-0 md:text-[20px] text-[17px] font-semibold font-serif"
